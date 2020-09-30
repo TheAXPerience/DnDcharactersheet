a12517b45a6479c04a20cd012a8786aec526b77d
Alvin <alvin3xu1@gmail.com>
2020-09-29 19:57:26 -0700

    yes

--- dndcharacter/characterpage/views.py
+++ dndcharacter/characterpage/views.py
@@ -77,19 +77,27 @@ def edit(request, character_id):
     }
     return render(request, 'characterpage/submit.html', context)
 
+def check_overflow(val):
+    val = int(val)
+    if val > 2147483647:
+        return 2147483647
+    elif val < -2147483648:
+        return -2147483648
+    return val
+
 def submit_edit(request, character_id):
     if request.is_ajax() and request.method == 'POST':
         data = json.loads(request.body)
         c = get_object_or_404(Character, pk=character_id)
         # very long if / elif chain bc python has no freakin' switch statements
         if (data['name'] == 'curr_hp'):
-            c.curr_hp = data['data']
+            c.curr_hp = check_overflow(data['data'])
         elif (data['name'] == 'temp_hp'):
-            c.temp_hp = data['data']
+            c.temp_hp = check_overflow(data['data'])
         elif (data['name'] == 'ds_success'):
-            c.ds_success = data['data']
+            c.ds_success = check_overflow(data['data'])
         elif (data['name'] == 'ds_failure'):
-            c.ds_failure = data['data']
+            c.ds_failure = check_overflow(data['data'])
         elif (data['name'] == 'CP'):
             c.cp = data['data']
         elif (data['name'] == 'EP'):
@@ -101,25 +109,23 @@ def submit_edit(request, character_id):
         elif (data['name'] == 'PP'):
             c.pp = data['data']
         elif (data['name'] == 'lv1'):
-            c.lv1_used = data['data']
+            c.lv1_used = check_overflow(data['data'])
         elif (data['name'] == 'lv2'):
-            c.lv2_used = data['data']
+            c.lv2_used = check_overflow(data['data'])
         elif (data['name'] == 'lv3'):
-            c.lv3_used = data['data']
+            c.lv3_used = check_overflow(data['data'])
         elif (data['name'] == 'lv4'):
-            c.lv4_used = data['data']
+            c.lv4_used = check_overflow(data['data'])
         elif (data['name'] == 'lv6'):
-            c.lv6_used = data['data']
+            c.lv6_used = check_overflow(data['data'])
         elif (data['name'] == 'lv5'):
-            c.lv5_used = data['data']
+            c.lv5_used = check_overflow(data['data'])
         elif (data['name'] == 'lv7'):
-            c.lv7_used = data['data']
+            c.lv7_used = check_overflow(data['data'])
         elif (data['name'] == 'lv8'):
-            c.lv8_used = data['data']
-        elif (data['name'] == 'lv1'):
-            c.lv1_used = data['data']
+            c.lv8_used = check_overflow(data['data'])
         elif (data['name'] == 'lv9'):
-            c.lv9_used = data['data']
+            c.lv9_used = check_overflow(data['data'])
         else:
             return HttpResponse('Value not found')
         c.date = timezone.now()
@@ -138,7 +144,13 @@ def submit_as(request, character_id):
             if len(atks) > 0:
                 return HttpResponse("Already exists")
             # make and save new attack/spell
-            a = AttackSpell(character=c, name=data['name'], hit_dc=data['hit'], r=data['range'], damage=data['damage'])
+            a = AttackSpell(
+                character=c,
+                name=data['name'][:30],
+                hit_dc=data['hit'][:10],
+                r=check_overflow(data['range']),
+                damage=data['damage'][:20]
+            )
             a.save()
             c.date = timezone.now()
             c.save()
@@ -158,7 +170,12 @@ def submit_eq(request, character_id):
             if len(eqs) > 0:
                 return HttpResponse("Already exists")
             # make and save new attack/spell
-            e = Equipment(character=c, name=data['name'], quantity=data['quantity'], weight=data['weight'])
+            e = Equipment(
+                character=c,
+                name=data['name'][:50],
+                quantity=check_overflow(data['quantity']),
+                weight=data['weight']
+            )
             e.save()
             c.date = timezone.now()
             c.save()
@@ -179,7 +196,12 @@ def submit_sp(request, character_id):
                 return HttpResponse("Already exists")
             # make and save new attack/spell
             p = (data['prepared'] == 'true')
-            s = CharSpells(character=c, name=data['name'], level=data['level'], prepared=p)
+            s = CharSpells(
+                character=c,
+                name=data['name'][:30],
+                level=check_overflow(data['level']),
+                prepared=p
+            )
             s.save()
             c.date = timezone.now()
             c.save()
@@ -188,110 +210,112 @@ def submit_sp(request, character_id):
     return HttpResponse("OK")
 
 def create(request):
-    return HttpResponse('create mode')
+    c = Character(date = timezone.now())
+    c.save()
+    return HttpResponseRedirect(reverse('cpage:char_edit', args=(c.id,)))
 
 def send_edit(request, character_id):
     c = get_object_or_404(Character, pk=character_id)
-    c.name = request.POST.get('charname')
+    c.name = request.POST.get('charname')[:50]
     c.date = timezone.now()
-    c.level = request.POST.get('level')
-    c.char_class = request.POST.get('charclass')
-    c.race = request.POST.get('charrace')
-    c.creator = request.POST.get('player')
-    c.bg = request.POST.get('background')
-    c.alignment = request.POST.get('alignment')
-    c.exp = request.POST.get('exp')
-    c.strength = request.POST.get('str')
-    c.dexterity = request.POST.get('dex')
-    c.constitution = request.POST.get('con')
-    c.intelligence = request.POST.get('int')
-    c.wisdom = request.POST.get('wsd')
-    c.charisma = request.POST.get('chr')
+    c.level = check_overflow(request.POST.get('level'))
+    c.char_class = request.POST.get('charclass')[:20]
+    c.race = request.POST.get('charrace')[:20]
+    c.creator = request.POST.get('player')[:30]
+    c.bg = request.POST.get('background')[:20]
+    c.alignment = request.POST.get('alignment')[:20]
+    c.exp = check_overflow(request.POST.get('exp'))
+    c.strength = check_overflow(request.POST.get('str'))
+    c.dexterity = check_overflow(request.POST.get('dex'))
+    c.constitution = check_overflow(request.POST.get('con'))
+    c.intelligence = check_overflow(request.POST.get('int'))
+    c.wisdom = check_overflow(request.POST.get('wsd'))
+    c.charisma = check_overflow(request.POST.get('chr'))
     c.inspiration = request.POST.get('inspiration') == 'on'
-    c.proficiency_bonus = request.POST.get('prf')
-    c.pass_wsd = request.POST.get('passwsd')
-    c.ac = request.POST.get('armor')
-    c.initiative = request.POST.get('init')
-    c.movement = request.POST.get('move')
-    c.curr_hp = request.POST.get('currhp')
-    c.max_hp = request.POST.get('maxhp')
-    c.temp_hp = request.POST.get('temphp')
-    c.ds_success = request.POST.get('dss')
-    c.ds_failure = request.POST.get('dsf')
+    c.proficiency_bonus = check_overflow(request.POST.get('prf'))
+    c.pass_wsd = check_overflow(request.POST.get('passwsd'))
+    c.ac = check_overflow(request.POST.get('armor'))
+    c.initiative = check_overflow(request.POST.get('init'))
+    c.movement = check_overflow(request.POST.get('move'))
+    c.curr_hp = check_overflow(request.POST.get('currhp'))
+    c.max_hp = check_overflow(request.POST.get('maxhp'))
+    c.temp_hp = check_overflow(request.POST.get('temphp'))
+    c.ds_success = check_overflow(request.POST.get('dss'))
+    c.ds_failure = check_overflow(request.POST.get('dsf'))
     c.str_st_prof = request.POST.get('strst_prf') == 'on'
-    c.str_st = request.POST.get('strst')
+    c.str_st = check_overflow(request.POST.get('strst'))
     c.dex_st_prof = request.POST.get('dexst_prf') == 'on'
-    c.dex_st = request.POST.get('dexst')
+    c.dex_st = check_overflow(request.POST.get('dexst'))
     c.con_st_prof = request.POST.get('const_prf') == 'on'
-    c.con_st = request.POST.get('const')
+    c.con_st = check_overflow(request.POST.get('const'))
     c.int_st_prof = request.POST.get('intst_prf') == 'on'
-    c.int_st = request.POST.get('intst')
+    c.int_st = check_overflow(request.POST.get('intst'))
     c.wsd_st_prof = request.POST.get('wsdst_prf') == 'on'
-    c.wsd_st = request.POST.get('wsdst')
+    c.wsd_st = check_overflow(request.POST.get('wsdst'))
     c.chr_st_prof = request.POST.get('chrst_prf') == 'on'
-    c.chr_st = request.POST.get('chrst')
+    c.chr_st = check_overflow(request.POST.get('chrst'))
     c.acro_prf = request.POST.get('acro_prf') == 'on'
-    c.acro = request.POST.get('acro')
+    c.acro = check_overflow(request.POST.get('acro'))
     c.anha_prf = request.POST.get('anha_prf') == 'on'
-    c.anha = request.POST.get('anha')
+    c.anha = check_overflow(request.POST.get('anha'))
     c.arca_prf = request.POST.get('arca_prf') == 'on'
-    c.arca = request.POST.get('arca')
+    c.arca = check_overflow(request.POST.get('arca'))
     c.athl_prf = request.POST.get('athl_prf') == 'on'
-    c.athl = request.POST.get('athl')
+    c.athl = check_overflow(request.POST.get('athl'))
     c.decp_prf = request.POST.get('decp_prf') == 'on'
-    c.decp = request.POST.get('decp')
+    c.decp = check_overflow(request.POST.get('decp'))
     c.hist_prf = request.POST.get('hist_prf') == 'on'
-    c.hist = request.POST.get('hist')
+    c.hist = check_overflow(request.POST.get('hist'))
     c.insi_prf = request.POST.get('insi_prf') == 'on'
-    c.insi = request.POST.get('insi')
+    c.insi = check_overflow(request.POST.get('insi'))
     c.intm_prf = request.POST.get('intm_prf') == 'on'
-    c.intm = request.POST.get('intm')
+    c.intm = check_overflow(request.POST.get('intm'))
     c.invs_prf = request.POST.get('invs_prf') == 'on'
-    c.invs = request.POST.get('invs')
+    c.invs = check_overflow(request.POST.get('invs'))
     c.medi_prf = request.POST.get('medi_prf') == 'on'
-    c.medi = request.POST.get('medi')
+    c.medi = check_overflow(request.POST.get('medi'))
     c.natr_prf = request.POST.get('natr_prf') == 'on'
-    c.natr = request.POST.get('natr')
+    c.natr = check_overflow(request.POST.get('natr'))
     c.perf_prf = request.POST.get('perf_prf') == 'on'
-    c.perf = request.POST.get('perf')
+    c.perf = check_overflow(request.POST.get('perf'))
     c.perc_prf = request.POST.get('perc_prf') == 'on'
-    c.perc = request.POST.get('perc')
+    c.perc = check_overflow(request.POST.get('perc'))
     c.pers_prf = request.POST.get('pers_prf') == 'on'
-    c.pers = request.POST.get('pers')
+    c.pers = check_overflow(request.POST.get('pers'))
     c.rlgn_prf = request.POST.get('rlgn_prf') == 'on'
-    c.rlgn = request.POST.get('rlgn')
+    c.rlgn = check_overflow(request.POST.get('rlgn'))
     c.sloh_prf = request.POST.get('sloh_prf') == 'on'
-    c.sloh = request.POST.get('sloh')
+    c.sloh = check_overflow(request.POST.get('sloh'))
     c.stea_prf = request.POST.get('stea_prf') == 'on'
-    c.stea = request.POST.get('stea')
+    c.stea = check_overflow(request.POST.get('stea'))
     c.surv_prf = request.POST.get('surv_prf') == 'on'
-    c.surv = request.POST.get('surv')
+    c.surv = check_overflow(request.POST.get('surv'))
     c.cp = request.POST.get('cp')
     c.sp = request.POST.get('sp')
     c.ep = request.POST.get('ep')
     c.gp = request.POST.get('gp')
     c.pp = request.POST.get('pp')
-    c.spell_abl = request.POST.get('spl_abl')
-    c.spl_save = request.POST.get('spl_save')
-    c.spl_atk = request.POST.get('spl_atk')
-    c.lv1_used = request.POST.get('lv1_used')
-    c.lv1_slots = request.POST.get('lv1_ttl')
-    c.lv2_used = request.POST.get('lv2_used')
-    c.lv2_slots = request.POST.get('lv2_ttl')
-    c.lv3_used = request.POST.get('lv3_used')
-    c.lv3_slots = request.POST.get('lv3_ttl')
-    c.lv4_used = request.POST.get('lv4_used')
-    c.lv4_slots = request.POST.get('lv4_ttl')
-    c.lv5_used = request.POST.get('lv5_used')
-    c.lv5_slots = request.POST.get('lv5_ttl')
-    c.lv6_used = request.POST.get('lv6_used')
-    c.lv6_slots = request.POST.get('lv6_ttl')
-    c.lv7_used = request.POST.get('lv7_used')
-    c.lv7_slots = request.POST.get('lv7_ttl')
-    c.lv8_used = request.POST.get('lv8_used')
-    c.lv8_slots = request.POST.get('lv8_ttl')
-    c.lv9_used = request.POST.get('lv9_used')
-    c.lv9_slots = request.POST.get('lv9_ttl')
+    c.spell_abl = request.POST.get('spl_abl')[:10]
+    c.spl_save = check_overflow(request.POST.get('spl_save'))
+    c.spl_atk = check_overflow(request.POST.get('spl_atk'))
+    c.lv1_used = check_overflow(request.POST.get('lv1_used'))
+    c.lv1_slots = check_overflow(request.POST.get('lv1_ttl'))
+    c.lv2_used = check_overflow(request.POST.get('lv2_used'))
+    c.lv2_slots = check_overflow(request.POST.get('lv2_ttl'))
+    c.lv3_used = check_overflow(request.POST.get('lv3_used'))
+    c.lv3_slots = check_overflow(request.POST.get('lv3_ttl'))
+    c.lv4_used = check_overflow(request.POST.get('lv4_used'))
+    c.lv4_slots = check_overflow(request.POST.get('lv4_ttl'))
+    c.lv5_used = check_overflow(request.POST.get('lv5_used'))
+    c.lv5_slots = check_overflow(request.POST.get('lv5_ttl'))
+    c.lv6_used = check_overflow(request.POST.get('lv6_used'))
+    c.lv6_slots = check_overflow(request.POST.get('lv6_ttl'))
+    c.lv7_used = check_overflow(request.POST.get('lv7_used'))
+    c.lv7_slots = check_overflow(request.POST.get('lv7_ttl'))
+    c.lv8_used = check_overflow(request.POST.get('lv8_used'))
+    c.lv8_slots = check_overflow(request.POST.get('lv8_ttl'))
+    c.lv9_used = check_overflow(request.POST.get('lv9_used'))
+    c.lv9_slots = check_overflow(request.POST.get('lv9_ttl'))
     c.profs_lang = request.POST.get('prf_lang')
     c.feats_traits = request.POST.get('ft_trt')
     c.personality = request.POST.get('personality')
